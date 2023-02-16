845f8f3b037215d8b18f1301354d147ec05cc8de
ipmanlk <io@navinda.xyz>
2023-02-17 00:43:09 +0530

    fix: cors

--- pi-server/src/app.ts
+++ pi-server/src/app.ts
@@ -1,7 +1,15 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
+
+const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
 });
